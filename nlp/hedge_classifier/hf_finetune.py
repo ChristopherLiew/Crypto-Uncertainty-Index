ee212a2b8ed05c976b842477101889c874feb9da
christopher.liew <christopher.liew@bytedance.com>
2022-02-28 15:42:10 +0800

    Local testing + Updated  Huggingface Classifier with Ray Tune PBT

--- nlp/hedge_classifier/hf_finetune.py
+++ nlp/hedge_classifier/hf_finetune.py
@@ -20,6 +20,7 @@ from ray import tune
 from ray.tune import CLIReporter
 from ray.tune.schedulers import PopulationBasedTraining
 from typing import Dict, Optional
+from pathlib import Path
 from datetime import datetime
 from datasets import load_dataset
 from transformers import (
@@ -60,7 +61,7 @@ WANDB_DEFAULT_ARGS = {
 
 
 # Training and Tuning with SOTA PBT
-def tune_and_train_hf_clf(
+def train_pbt_hf_clf(
     model_name: str,
     train_data_dir: str,
     model_save_dir: str,
@@ -74,13 +75,16 @@ def tune_and_train_hf_clf(
     ray_address: Optional[str] = None,
     ray_num_trials: int = 8,  # Number of times to rand sample a point
 ) -> None:
+
     # Ray Tune set up
     ray.init(ray_address)  # Defaults to None = Local
+
     # WandB set up
     wandb.login()
     os.environ["WANDB_LOG_MODEL"] = "true"
     os.environ["WANDB_WATCH"] = "all"
     wandb_args["tags"].append(model_name)
+
     # Initialize WandB run (Only needed to log artifacts)
     run = wandb.init(
         job_type="training",
@@ -89,11 +93,14 @@ def tune_and_train_hf_clf(
         tags=wandb_args.get("tags", ["DummyTrainTag"]),
         entity=wandb_args["entity"],
     )
+
     # Check for CUDA
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
     # Get dataset
-    data_files = get_data_files(train_data_dir)
+    data_files = get_data_files(Path(train_data_dir))
     datasets = load_dataset(train_data_file_type, data_files=data_files)
+
     # Load Tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -102,6 +109,7 @@ def tune_and_train_hf_clf(
 
     # Tokenize Datasets
     tokenized_datasets = datasets.map(tokenizer_function, batched=True)
+
     # Final Datasets
     sample_train_ds = (
         tokenized_datasets["train"].shuffle(seed=42).select(range(sample_data_size))
@@ -111,12 +119,20 @@ def tune_and_train_hf_clf(
     )
     train_ds = tokenized_datasets["train"]
     test_ds = tokenized_datasets["test"]
+
     # Data Collator with Padding (Dynamic Padding)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    # Load Pretrained Model
-    model = AutoModelForSequenceClassification.from_pretrained(
+
+    # Download Pretrained Model
+    AutoModelForSequenceClassification.from_pretrained(
         model_name, num_labels=num_labels
     )
+
+    # Get Pretrained Modelfor Ray Hyperparam Tuning (must pass as func)
+    def get_model() -> AutoModelForSequenceClassification:
+        return AutoModelForSequenceClassification.from_pretrained(
+            model_name, num_labels=num_labels)
+
     # Training Config
     train_args = TrainingArguments(
         report_to="wandb",
@@ -125,7 +141,7 @@ def tune_and_train_hf_clf(
         load_best_model_at_end=True,
         evaluation_strategy="steps",  # Eval often to prune bad trials
         eval_steps=500,
-        save_strategy="epoch",
+        save_strategy="steps",
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=5,
@@ -133,13 +149,15 @@ def tune_and_train_hf_clf(
         learning_rate=2e-5,
         weight_decay=0.01,
         logging_steps=100,
-        logging_dir="./logs",
+        logging_dir="./nlp/hedge_classifier/logs",
     )
+
     # Callbacks
     tb_callback = TensorBoardCallback()
+
     # Trainer
     trainer = Trainer(
-        model=model,
+        model_init=get_model,
         args=train_args,
         train_dataset=sample_train_ds if sample_data_size else train_ds,
         eval_dataset=sample_test_ds if sample_data_size else test_ds,
@@ -147,6 +165,7 @@ def tune_and_train_hf_clf(
         compute_metrics=compute_clf_metrics,
         callbacks=[tb_callback],
     )
+
     # PBT Hyperparam Search with RayTune
     tune_config = {
         "per_device_train_batch_size": 32,
@@ -154,6 +173,7 @@ def tune_and_train_hf_clf(
         "num_train_epochs": tune.choice([2, 3, 4, 5]),
         "max_steps": 1 if smoke_test else -1,  # Used for smoke test.
     }
+
     scheduler = PopulationBasedTraining(
         time_attr="training_iteration",
         metric="eval_acc",
@@ -165,6 +185,7 @@ def tune_and_train_hf_clf(
             "per_device_train_batch_size": [16, 32, 64],
         },
     )
+
     reporter = CLIReporter(
         parameter_columns={
             "weight_decay": "w_decay",
@@ -174,6 +195,7 @@ def tune_and_train_hf_clf(
         },
         metric_columns=["eval_acc", "eval_loss", "epoch", "training_iteration"],
     )
+
     best_trial = trainer.hyperparameter_search(
         hp_space=lambda _: tune_config,
         backend="ray",
@@ -184,10 +206,11 @@ def tune_and_train_hf_clf(
         checkpoint_score_attr="training_iteration",
         stop={"training_iteration": 1} if smoke_test else None,
         progress_reporter=reporter,
-        local_dir="./ray_results/",
-        name="tune_transformer_pbt",
+        local_dir="./nlp/hedge_classifier/ray_results/",
+        name="tune_bertweet_pbt",
         log_to_file=True,
     )
+
     # End WandB Session
     wandb.finish()
     # Serialize Best Trial
@@ -195,3 +218,31 @@ def tune_and_train_hf_clf(
 
 
 # Test
+# if __name__ == "__main__":
+#     model_name = "vinai/bertweet-base"
+#     train_data_dir = "nlp/hedge_classifier/data/wiki_weasel_clean"
+#     model_save_dir = "nlp/hedge_classifier/models"
+#     num_labels = 2
+#     text_col = "text"
+#     wandb_args = WANDB_DEFAULT_ARGS
+#     train_data_file_type = "csv"
+#     sample_data_size = 1000
+#     num_gpus_per_trial = 0
+#     smoke_test = False
+#     ray_address = None
+#     ray_num_trials = 8
+
+#     train_pbt_hf_clf(
+#         model_name=model_name,
+#         train_data_dir=train_data_dir,
+#         model_save_dir=model_save_dir,
+#         num_labels=num_labels,
+#         text_col=text_col,
+#         wandb_args=wandb_args,
+#         train_data_file_type=train_data_file_type,
+#         sample_data_size=sample_data_size,
+#         num_gpus_per_trial=num_gpus_per_trial,
+#         smoke_test=smoke_test,
+#         ray_address=ray_address,
+#         ray_num_trials=ray_num_trials
+#     )
